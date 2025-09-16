ae3363c1aed1a1c9715cf22f7603e0fac12566f1
ebicthings <ebicthings123@gmail.com>
2025-09-16 21:17:39 +0800

    Added again and reformed

--- Acepack.lua
+++ Acepack.lua
@@ -1,41 +1,41 @@
-
 --ICON
 SMODS.Atlas({
-    key = "modicon", 
-    path = "ModIcon.png", 
+    key = "modicon",
+    path = "ModIcon.png",
     px = 34,
     py = 34,
     atlas_table = "ASSET_ATLAS"
 })
 
 SMODS.Atlas { --jokers
-	-- Key for code to find it with
-	key = "CustomJokers",
-	-- The name of the file, for the code to pull the atlas from
-	path = "CustomJokers.png",
-	-- Width of each sprite in 1x size
-	px = 71,
-	-- Height of each sprite in 1x size
-	py = 95
+    -- Key for code to find it with
+    key = "CustomJokers",
+    -- The name of the file, for the code to pull the atlas from
+    path = "CustomJokers.png",
+    -- Width of each sprite in 1x size
+    px = 71,
+    -- Height of each sprite in 1x size
+    py = 95
 }
 
 --SOUNDS
-SMODS.Sound{--for 6shooter
-    key="dies",
-    path="dies.ogg",
-    pitch=0.7,
-    volume=0.6
+SMODS.Sound { --for 6shooter
+    key = "dies",
+    path = "dies.ogg",
+    pitch = 0.7,
+    volume = 0.6
 }
 
-SMODS.Sound{--for kallamar
-    key="aww",
-    path="aww.ogg",
-    pitch=0.7,
-    volume=0.6
+SMODS.Sound { --for kallamar
+    key = "aww",
+    path = "aww.ogg",
+    pitch = 0.7,
+    volume = 0.6
 }
 
-SMODS.Rarity {
+SMODS.Rarity { --generika
     key = "generika",
+    loc_txt = { name = "Generika" },
     default_weight = 0.15,
     badge_colour = HEX('748ca3'),
     get_weight = function(self, weight, object_type)
@@ -43,9 +43,13 @@ SMODS.Rarity {
     end,
 }
 
+
+
+
+
 --JOKERS
 
-SMODS.Joker{ --Ace
+SMODS.Joker {   --Ace
     key = "ace",
     loc_txt = { --basically all the info and description
         name = 'Ace',
@@ -59,13 +63,13 @@ SMODS.Joker{ --Ace
             'Unlocked by default.'
         }
     },
-        config = {
+    config = {
         extra = {
             repetitions = 1,
             Xmult = 3
         }
     },
-    
+
     pos = {
         x = 2,
         y = 0
@@ -83,7 +87,7 @@ SMODS.Joker{ --Ace
 
 
     calculate = function(self, card, context)
-        if context.repetition and context.cardarea == G.play  then
+        if context.repetition and context.cardarea == G.play then
             if context.other_card:get_id() == 14 then
                 return {
                     repetitions = card.ability.extra.repetitions,
@@ -91,7 +95,7 @@ SMODS.Joker{ --Ace
                 }
             end
         end
-        if context.individual and context.cardarea == G.play  then
+        if context.individual and context.cardarea == G.play then
             if context.other_card:get_id() == 14 then
                 return {
                     Xmult = card.ability.extra.Xmult
@@ -101,7 +105,7 @@ SMODS.Joker{ --Ace
     end
 }
 
-SMODS.Joker{ --Shipping wall
+SMODS.Joker { --Shipping wall
     key = "shipping_wall",
     loc_txt = {
         name = 'Shipping Wall',
@@ -113,63 +117,61 @@ SMODS.Joker{ --Shipping wall
             [1] = 'Unlocked by default.'
         }
     },
-    config = { extra = { Xmult=2} },
+    config = { extra = { Xmult = 2 } },
     rarity = 2,
     atlas = 'CustomJokers',
-    pos = { x=4, y=1 },
+    pos = { x = 4, y = 1 },
     cost = 9,
     blueprint_compat = true,
     unlocked = true,
     discovered = true,
-    
+
     calculate = function(self, card, context)
-    if context.joker_main then
+        if context.joker_main then
             local suits = {
-            ['Hearts'] = 0,
-            ['Diamonds'] = 0,
-            ['Spades'] = 0,
-            ['Clubs'] = 0
-        }
-
-        for i = 1, #context.scoring_hand do
-            if not SMODS.has_any_suit(context.scoring_hand[i]) then
-                if context.scoring_hand[i]:is_suit('Hearts', true) and suits["Hearts"] == 0 then
-                    suits["Hearts"] = suits["Hearts"] + 1
-                elseif context.scoring_hand[i]:is_suit('Diamonds', true) and suits["Diamonds"] == 0 then
-                    suits["Diamonds"] = suits["Diamonds"] + 1
-                elseif context.scoring_hand[i]:is_suit('Spades', true) and suits["Spades"] == 0 then
-                    suits["Spades"] = suits["Spades"] + 1
-                elseif context.scoring_hand[i]:is_suit('Clubs', true) and suits["Clubs"] == 0 then
-                    suits["Clubs"] = suits["Clubs"] + 1
-                end
-            end
-        end
-        for i = 1, #context.scoring_hand do
-            if SMODS.has_any_suit(context.scoring_hand[i]) then
-                if context.scoring_hand[i]:is_suit('Hearts') and suits["Hearts"] == 0 then
-                    suits["Hearts"] = suits["Hearts"] + 1
-                elseif context.scoring_hand[i]:is_suit('Diamonds') and suits["Diamonds"] == 0 then
-                    suits["Diamonds"] = suits["Diamonds"] + 1
-                elseif context.scoring_hand[i]:is_suit('Spades') and suits["Spades"] == 0 then
-                    suits["Spades"] = suits["Spades"] + 1
-                elseif context.scoring_hand[i]:is_suit('Clubs') and suits["Clubs"] == 0 then
-                    suits["Clubs"] = suits["Clubs"] + 1
-                end
-            end
-        end
-        if (suits["Hearts"] > 0 and suits["Diamonds"] > 0 and suits["Spades"] == 0 and suits["Clubs"] == 0) or
-            (suits["Spades"] > 0 and suits["Clubs"] > 0 and suits["Hearts"] == 0 and suits["Diamonds"] == 0) then
-            return {
-                xmult = card.ability.extra.Xmult
+                ['Hearts'] = 0,
+                ['Diamonds'] = 0,
+                ['Spades'] = 0,
+                ['Clubs'] = 0
             }
-        end
-        
 
+            for i = 1, #context.scoring_hand do
+                if not SMODS.has_any_suit(context.scoring_hand[i]) then
+                    if context.scoring_hand[i]:is_suit('Hearts', true) and suits["Hearts"] == 0 then
+                        suits["Hearts"] = suits["Hearts"] + 1
+                    elseif context.scoring_hand[i]:is_suit('Diamonds', true) and suits["Diamonds"] == 0 then
+                        suits["Diamonds"] = suits["Diamonds"] + 1
+                    elseif context.scoring_hand[i]:is_suit('Spades', true) and suits["Spades"] == 0 then
+                        suits["Spades"] = suits["Spades"] + 1
+                    elseif context.scoring_hand[i]:is_suit('Clubs', true) and suits["Clubs"] == 0 then
+                        suits["Clubs"] = suits["Clubs"] + 1
+                    end
+                end
+            end
+            for i = 1, #context.scoring_hand do
+                if SMODS.has_any_suit(context.scoring_hand[i]) then
+                    if context.scoring_hand[i]:is_suit('Hearts') and suits["Hearts"] == 0 then
+                        suits["Hearts"] = suits["Hearts"] + 1
+                    elseif context.scoring_hand[i]:is_suit('Diamonds') and suits["Diamonds"] == 0 then
+                        suits["Diamonds"] = suits["Diamonds"] + 1
+                    elseif context.scoring_hand[i]:is_suit('Spades') and suits["Spades"] == 0 then
+                        suits["Spades"] = suits["Spades"] + 1
+                    elseif context.scoring_hand[i]:is_suit('Clubs') and suits["Clubs"] == 0 then
+                        suits["Clubs"] = suits["Clubs"] + 1
+                    end
+                end
+            end
+            if (suits["Hearts"] > 0 and suits["Diamonds"] > 0 and suits["Spades"] == 0 and suits["Clubs"] == 0) or
+                (suits["Spades"] > 0 and suits["Clubs"] > 0 and suits["Hearts"] == 0 and suits["Diamonds"] == 0) then
+                return {
+                    xmult = card.ability.extra.Xmult
+                }
+            end
+        end
     end
-end
 }
 
-SMODS.Joker{ --Kallamar aka my wife
+SMODS.Joker { --Kallamar aka my wife
     key = "kallamar",
     config = {
         extra = {
@@ -196,7 +198,7 @@ SMODS.Joker{ --Kallamar aka my wife
         y = 1
     },
     display_size = {
-        w = 71 * 1, 
+        w = 71 * 1,
         h = 95 * 1
     },
     cost = 10,
@@ -212,20 +214,21 @@ SMODS.Joker{ --Kallamar aka my wife
         y = 1
     },
     in_pool = function(self, args)
-          return (
-          not args 
-          or args.source ~= 'sho' 
-          or args.source == 'buf' or args.source == 'jud' or args.source == 'rif' or args.source == 'rta' or args.source == 'sou' or args.source == 'uta' or args.source == 'wra'
-          )
-          and true
-      end,
+        return (
+                not args
+                or args.source ~= 'sho'
+                or args.source == 'buf' or args.source == 'jud' or args.source == 'rif' or args.source == 'rta' or args.source == 'sou' or args.source == 'uta' or args.source == 'wra'
+            )
+            and true
+    end,
 
     calculate = function(self, card, context)
         if context.other_joker and context.other_joker.config.center.key ~= "j_AcePack_kallamar" then
             if (function()
-        return context.other_joker.config.center.key == "j_AcePack_ace" end)() then
+                    return context.other_joker.config.center.key == "j_AcePack_ace"
+                end)() then
                 return {
-                    
+
                     Xmult = card.ability.extra.Xmult,
                     message = "Love wins",
                     sound = "AcePack_aww" --IT WAS THAT EASY?!?!?!?!?!??
@@ -239,16 +242,16 @@ SMODS.Joker{ --Kallamar aka my wife
     end
 }
 
-SMODS.Joker{ --Six Shooter
+SMODS.Joker { --Six Shooter
     key = "six_shooter",
     config = {
-    extra = {
-        deathin = 5,
-        Xmult = 2.6,
-        Xmult2 = 0,
-        AcePack_dies = 0
-    }
-},
+        extra = {
+            deathin = 5,
+            Xmult = 2.6,
+            Xmult2 = 0,
+            AcePack_dies = 0
+        }
+    },
     loc_txt = {
         name = 'Six Shooter',
         text = {
@@ -268,24 +271,24 @@ SMODS.Joker{ --Six Shooter
     perishable_compat = true,
     unlocked = true,
     discovered = true,
-    pos = {x = 0,y = 0},
-    soul_pos = {x=1,y=0},
+    pos = { x = 0, y = 0 },
+    soul_pos = { x = 1, y = 0 },
     loc_vars = function(self, info_queue, card)
-        return {vars = {card.ability.extra.deathin}}
+        return { vars = { card.ability.extra.deathin } }
     end,
     calculate = function(self, card, context)
-        if context.cardarea == G.jokers and context.joker_main  then
-                card.ability.extra.deathin = math.max(0, (card.ability.extra.deathin) - 1)
-                return {
-                    extra = {
-                        Xmult = card.ability.extra.Xmult
-                        }
+        if context.cardarea == G.jokers and context.joker_main then
+            card.ability.extra.deathin = math.max(0, (card.ability.extra.deathin) - 1)
+            return {
+                extra = {
+                    Xmult = card.ability.extra.Xmult
                 }
+            }
         end
-        if context.individual and context.cardarea == G.play  then
+        if context.individual and context.cardarea == G.play then
             if (context.other_card == context.scoring_hand[#context.scoring_hand] and (card.ability.extra.deathin or 0) <= 0) then
                 card.ability.extra.deathin = 6
-                
+
                 return {
                     Xmult = card.ability.extra.Xmult2,
                     message = "Bang!",
@@ -297,7 +300,7 @@ SMODS.Joker{ --Six Shooter
 
 }
 
-SMODS.Joker{ --Again?
+SMODS.Joker { --Again?
     key = "again",
     config = {
         extra = { repetitions = 3
@@ -325,18 +328,18 @@ SMODS.Joker{ --Again?
     discovered = true,
     atlas = 'CustomJokers',
     calculate = function(self, card, context)
-        if context.repetition and context.cardarea == G.play  then
-                return {
-                    repetitions = card.ability.extra.repetitions,
-                    message = "Again!"
-                }
+        if context.repetition and context.cardarea == G.play then
+            return {
+                repetitions = card.ability.extra.repetitions,
+                message = "Again!"
+            }
         end
     end
-    
+
 }
-SMODS.Joker{ --rekoj
+SMODS.Joker { --rekoj
     key = "rekoj",
-    loc_txt ={
+    loc_txt = {
         name = 'Rekoj',
         text = {
             '{X:blue,C:white}+400{} {C:blue}Chips {}',
@@ -347,28 +350,28 @@ SMODS.Joker{ --rekoj
             'Unlocked by default.'
         }
     },
-    pos = {x=3, y=1},
+    pos = { x = 3, y = 1 },
     cost = 8,
     rarity = "AcePack_generika",
     config = {
-    extra = {
-        chips = 400
-    }
+        extra = {
+            chips = 400
+        }
     },
     blueprint_compat = true,
     unlocked = true,
     discovered = true,
     atlas = 'CustomJokers',
     calculate = function(self, card, context)
-        if context.joker_main  then
-                return {
-                    chips = card.ability.extra.chips
-                }
+        if context.joker_main then
+            return {
+                chips = card.ability.extra.chips
+            }
         end
     end
 
 }
-SMODS.Joker{ --Four-eyes
+SMODS.Joker { --Four-eyes
     key = "foureyes",
     config = {
         extra = {
@@ -397,11 +400,97 @@ SMODS.Joker{ --Four-eyes
     discovered = true,
     atlas = 'CustomJokers',
     calculate = function(self, card, context)
-        if context.joker_main  then
-                return {
-                    Xmult = card.ability.extra.Xmult
-                }
+        if context.joker_main then
+            return {
+                Xmult = card.ability.extra.Xmult
+            }
         end
     end
 }
 
+SMODS.Joker { --Again?
+    key = "again",
+    config = { extra = { repetitions = 3 } },
+    loc_txt = {
+        name = 'Again?',
+        text = {
+            'Retriggers all cards three times'
+        },
+        unlock = {
+            'Unlocked by default.'
+        }
+    },
+    pos = { x = 4, y = 0 },
+    cost = 5,
+    rarity = "AcePack_generika",
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    unlocked = true,
+    discovered = true,
+    atlas = 'CustomJokers',
+    calculate = function(self, card, context)
+        if context.repetition and context.cardarea == G.play then
+            return {
+                repetitions = card.ability.extra.repetitions,
+                message = "Again?"
+            }
+        end
+    end
+}
+
+SMODS.Joker { --reformed
+    key = "reformed",
+    config = { extra = { Saneness = 1 } },
+    loc_txt = {
+        name = 'Reformed',
+        text = {
+            '{X:red,C:white}x#1# Mult{}',
+            '{C:red}+0.25{} Mult for every {C:enhanced}Enhanced{} playing card destroyed'
+
+        },
+        unlock = {
+            'Unlocked by default.'
+        }
+    },
+    pos = { x = 2, y = 1 },
+    cost = 5,
+    rarity = "AcePack_generika",
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    unlocked = true,
+    discovered = true,
+    atlas = 'CustomJokers',
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.Saneness } }
+    end,
+    calculate = function(self, card, context)
+        if context.cardarea == G.jokers and context.joker_main then
+            return {
+                Xmult = card.ability.extra.Saneness
+            }
+        end
+        if context.remove_playing_cards then
+            if (function()
+                    for k, removed_card in ipairs(context.removed) do
+                        local enhancements = SMODS.get_enhancements(removed_card)
+                        for k, v in pairs(enhancements) do
+                            if v then
+                                return true
+                            end
+                        end
+                    end
+                    return false
+                end)() then
+                return {
+                    func = function()
+                        card.ability.extra.Saneness = (card.ability.extra.Saneness) + 0.25
+                        return true
+                    end,
+                    message = "Cleansed"
+                }
+            end
+        end
+    end
+}
